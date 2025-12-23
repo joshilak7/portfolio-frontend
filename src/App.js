98eaea699ffb10f4ed57058cd652fc1d6569de21
Your Name <you@example.com>
2025-12-23 15:46:51 +0530

    Nextt

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
 
 function App() {
